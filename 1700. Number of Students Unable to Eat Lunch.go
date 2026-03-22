2bb54de2b770b3d29b777b917f544ff1657c5d8f
bahtiyorovkamron <bahtiyorovkamron@gmail.com>
2026-03-22 12:44:58 +0500

    1700 the problem

---
+++ 1700. Number of Students Unable to Eat Lunch.go
@@ -0,0 +1,30 @@
+package main
+
+func CountStudents(students []int, sandwiches []int) int {
+
+	st := make(map[int]int)
+
+	for i := range students {
+		st[students[i]]++
+	}
+
+	for true {
+		if len(students) == 0 {
+			return 0
+		}
+		if ok := st[sandwiches[0]]; ok == 0 {
+			return len(students)
+		}
+		if students[0] == sandwiches[0] {
+			k := students[0]
+			students = students[1:]
+			sandwiches = sandwiches[1:]
+			st[k]--
+		} else {
+			kaprizniy := students[0]
+			students = students[1:]
+			students = append(students, kaprizniy)
+		}
+	}
+	return 0
+}
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
 
 	// n4 := &ListNode{
 	// 	Val:  1,
@@ -31,9 +30,7 @@ func main() {
 
 	// fmt.Println(a[0], a[1], a[2], a[3])
 
-	fmt.Println(MinLength("AATQCABDCBE"))
-=======
+	fmt.Println(CountStudents([]int{1,1,1,0,0,1}, []int{1,0,0,0,1,1}))
 	//ExclusiveTime(1, []string{"0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7"})
-	fmt.Println(MinOperations([]string{"./", "wz4/", "../", "mj2/", "../", "../", "ik0/", "il7/"}))
->>>>>>> a4295e0d6c2b2989d6cd6aec1d9058f6ad1e5275
+	// fmt.Println(MinOperations([]string{"./", "wz4/", "../", "mj2/", "../", "../", "ik0/", "il7/"}))
 }
